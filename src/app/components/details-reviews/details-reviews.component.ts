c34ac610587929c12745ff928b197e45141cc4c2
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-04-30 01:59:31 -0300

    feat(details-reviews-ts): create data to be shown

--- src/app/components/details-reviews/details-reviews.component.ts
+++ src/app/components/details-reviews/details-reviews.component.ts
@@ -3,13 +3,36 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-details-reviews',
   templateUrl: './details-reviews.component.html',
-  styleUrls: ['./details-reviews.component.css']
+  styleUrls: ['./details-reviews.component.css'],
 })
 export class DetailsReviewsComponent implements OnInit {
+  movieRates: any[] = [
+    {
+      rate: 7.5,
+      review: 'This is just an example of how this text could be in a review!',
+      author: 'Author',
+      usefulCount: 100,
+      totalCount: 120,
+      id: 1,
+    },
+    {
+      rate: 9.5,
+      review: 'This is just an example of how this text could be in a review!',
+      author: 'Author',
+      usefulCount: 200,
+      totalCount: 320,
+      id: 2,
+    },
+    {
+      rate: 8.5,
+      review: 'This is just an example of how this text could be in a review!',
+      author: 'Author',
+      usefulCount: 110,
+      totalCount: 120,
+      id: 3,
+    },
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
