3a0ae856c4625bbc4efe26c9e47a2ce06928762f
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-04-30 02:04:50 -0300

    feat(details-movies-ts): create data to be shown

--- src/app/components/details-movies/details-movies.component.ts
+++ src/app/components/details-movies/details-movies.component.ts
@@ -3,13 +3,43 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-details-movies',
   templateUrl: './details-movies.component.html',
-  styleUrls: ['./details-movies.component.css']
+  styleUrls: ['./details-movies.component.css'],
 })
 export class DetailsMoviesComponent implements OnInit {
+  similarMovies: any[] = [
+    {
+      imageUrl: '../../../assets/images/pic.jpg',
+      title: 'Movie Title',
+      rate: 9.5,
+    },
+    {
+      imageUrl: '../../../assets/images/pic.jpg',
+      title: 'Movie Title',
+      rate: 9.5,
+    },
+    {
+      imageUrl: '../../../assets/images/pic.jpg',
+      title: 'Movie Title',
+      rate: 9.5,
+    },
+    {
+      imageUrl: '../../../assets/images/pic.jpg',
+      title: 'Movie Title',
+      rate: 9.5,
+    },
+    {
+      imageUrl: '../../../assets/images/pic.jpg',
+      title: 'Movie Title',
+      rate: 9.5,
+    },
+    {
+      imageUrl: '../../../assets/images/pic.jpg',
+      title: 'Movie Title',
+      rate: 9.5,
+    },
 
-  constructor() { }
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
