41059de527598609ed4bcb434a01586f61551cb5
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-04-30 01:58:30 -0300

    feat(details-ts): create data to be shown

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -17,10 +17,14 @@ export class DetailsComponent
 {
   movieId = '';
 
+  stars = ['Star #1 Name', 'Star #2 Name', 'Star #3 Name'];
+  directors = ['Directors #1', 'Directors #2 ', 'Directors #3 '];
+  genres = ['Genre #1 Name', 'Genre #2 Name', 'Genre #3 Name'];
+
   constructor(private _activedRoute: ActivatedRoute) {
     this._activedRoute.params.subscribe((p) => {
       this.movieId = p['id'];
-      console.log(this.movieId)
+      console.log(this.movieId);
     });
   }
 
