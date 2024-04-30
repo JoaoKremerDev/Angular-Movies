17af7089716a7f57f7a0f8e5675b80468ed1cb5e
JoaoKremerDev <joaokremer.dev@gmail.com>
2024-04-30 15:15:43 -0300

    feat(movies-service): create a service to get movie's request

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,10 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class MoviesService {
+  constructor(private httpClient: HttpClient) {}
 
-  constructor() { }
+  getFanFavoriteMovies() {
+    return this.httpClient.get<any[]>('assets/data/fanFavoriteMovies.json');
+  }
+
+  getTopMovies() {
+    return this.httpClient.get<any[]>('assets/data/topMovies.json');
+  }
+
+  getLoadMovieSummary() {
+    return this.httpClient.get('assets/data/movieSummary.json');
+  }
+
+  getLoadMovieActors() {
+    return this.httpClient.get<any[]>('assets/data/movieActors.json');
+  }
+
+  getLoadSimilarMovies() {
+    return this.httpClient.get('assets/data/similarMovies.json');
+  }
+
+  getLoadReviews() {
+    return this.httpClient.get('assets/data/movieRates.json');
+  }
+
+  getSearchedMovies() {
+    return this.httpClient.get('assets/data/movieSearch.json');
+  }
 }
